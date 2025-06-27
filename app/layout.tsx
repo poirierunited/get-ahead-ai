a4fffd1ff1de7d09c185e21826a670d510ff95aa
Alejandro Poirier <ale.poirier16@gmail.com>
2025-06-26 23:39:27 -0400

    Refactor layout and enhance AuthForm component
    - Updated layout.tsx to include the Toaster component and improved import formatting.
    - Redesigned AuthForm component to utilize react-hook-form for form handling and validation, incorporating Zod for schema validation.
    - Added FormField component for reusable form input fields, enhancing code modularity and readability.
    - Improved user feedback with toast notifications for sign-in and sign-up actions.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
-import type { Metadata } from "next";
-import { Mona_Sans } from "next/font/google";
-
-import "./globals.css";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Mona_Sans } from 'next/font/google';
+import { Toaster } from 'sonner';
 
 const monaSans = Mona_Sans({
-  variable: "--font-mona-sans",
-  subsets: ["latin"],
+  variable: '--font-mona-sans',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "The Get Ahead Project",
-  description: "Plataforma de preparación para mercado laboral impulsado por Inteligencia Artificial",
+  title: 'The Get Ahead Project',
+  description:
+    'Plataforma de preparación para mercado laboral impulsado por Inteligencia Artificial',
 };
 
 export default function RootLayout({
@@ -19,11 +20,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body
-        className={`${monaSans.className} antialiased pattern`}
-      >
+    <html lang='en' className='dark'>
+      <body className={`${monaSans.className} antialiased pattern`}>
         {children}
+
+        <Toaster />
       </body>
     </html>
   );
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,15 +1,116 @@
-import React from 'react'
+'use client';
 
-interface AuthFormProps {
-    type: 'sign-in' | 'sign-up'
-}
+import Image from 'next/image';
+import Link from 'next/link';
 
-const AuthForm = ({ type }: AuthFormProps) => {
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import { Form } from '@/components/ui/form';
+import FormField from '@/components/FormField';
+import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
+
+type FormType = 'sign-in' | 'sign-up';
+
+const authFormSchema = (type: FormType) => {
+  return z.object({
+    name: type === 'sign-up' ? z.string().min(3) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(8),
+  });
+};
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  const router = useRouter();
+  const formSchema = authFormSchema(type);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      if (type === 'sign-up') {
+        toast.success('Account created successfully. Please sign in.');
+        router.push('/sign-in');
+      } else {
+        toast.success('Signed in successfully');
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(`Something went wrong: ${error}`);
+    }
+  }
+
+  const isSignIn = type === 'sign-in';
+
   return (
-    <div>
-      {type === 'sign-in' ? 'Sign In Form' : 'Sign Up Form'}
-    </div>
-  )
-}
+    <div className='card-border lg:min-w-[566px]'>
+      <div className='flex flex-col gap-6 card py-14 px-10'>
+        <div className='flex flex-row gap-2 justify-center'>
+          <Image src='/logo.svg' alt='logo' width={38} height={32} />
+          <h2 className='text-primary-100'>PrepWise</h2>
+        </div>
 
-export default AuthForm
+        <h3>Practice job interview with AI</h3>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='w-full space-y-6 mt-4 form'
+          >
+            {!isSignIn && (
+              <FormField
+                control={form.control}
+                name='name'
+                label='Name'
+                placeholder='Your Name'
+                type='text'
+              />
+            )}
+
+            <FormField
+              control={form.control}
+              name='email'
+              label='Email'
+              placeholder='Your email address'
+              type='email'
+            />
+
+            <FormField
+              control={form.control}
+              name='password'
+              label='Password'
+              placeholder='Enter your password'
+              type='password'
+            />
+
+            <Button className='btn' type='submit'>
+              {isSignIn ? 'Sign in' : 'Create an account'}
+            </Button>
+          </form>
+        </Form>
+        <p className='text-center'>
+          {isSignIn ? "Don't have an account?" : 'Already have an account?'}
+          <Link
+            href={!isSignIn ? '/sign-in' : '/sign-up'}
+            className='font-bold text-user-primary ml-1'
+          >
+            {!isSignIn ? 'Sign in' : 'Sign up'}
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
