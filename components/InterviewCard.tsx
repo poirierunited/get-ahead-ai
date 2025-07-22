58ad89f5957ccd2c7782aa23da5b08a7e284f795
Alejandro Poirier <ale.poirier16@gmail.com>
2025-07-22 09:14:31 -0400

    feat: add DisplayTechIcons component and enhance InterviewCard
    - Introduced DisplayTechIcons component to display technology icons based on the tech stack.
    - Integrated DisplayTechIcons into the InterviewCard component for improved visual representation of technologies used in interviews.
    - Updated utility functions to fetch and validate tech logos dynamically.
    - Added new actions for user authentication and feedback management.

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import dayjs from 'dayjs';
-import Image from 'next/image';
+import React from "react";
+import dayjs from "dayjs";
+import Image from "next/image";
+import { getRandomInterviewCover } from "@/lib/utils";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import DisplayTechIcons from "./DisplayTechIcons";
 
 const InterviewCard = ({
   interviewId,
@@ -11,27 +15,68 @@ const InterviewCard = ({
   createdAt,
 }: InterviewCardProps) => {
   const feedback = null as Feedback | null;
-  const normalizedType = /mix/gi.test(type) ? 'mixed' : type;
+  const normalizedType = /mix/gi.test(type) ? "mixed" : type;
 
   const formattedDate = dayjs(
     feedback?.createdAt || createdAt || Date.now()
-  ).format('DD MMM YYYY');
+  ).format("DD MMM YYYY");
 
   return (
-    <div className='card-border w-[360px] max-sm:w-full min-h-96'>
-      <div className='card-interview'>
+    <div className="card-border w-[360px] max-sm:w-full min-h-96">
+      <div className="card-interview">
         <div>
-          <div className='absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600'>
-            <p className='badge-text'>{normalizedType}</p>
+          <div className="absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600">
+            <p className="badge-text">{normalizedType}</p>
           </div>
 
           <Image
             src={getRandomInterviewCover()}
-            alt='cover'
+            alt="cover"
             width={90}
             height={90}
-            className='rounded-full object-fit size-[90px]'
+            className="rounded-full object-fit size-[90px]"
           />
+
+          <h3 className="mt-5 capitalize">{role} Interview</h3>
+
+          <div className="flex flex-row gap-5 mt-3">
+            <div className="flex flex-row gap-2">
+              <Image
+                src={"/calendar.svg"}
+                alt="calendar"
+                width={22}
+                height={22}
+              />
+              <p className="text-sm text-light-500">{formattedDate}</p>
+            </div>
+
+            <div className="flex flex-row gap-2 items-center">
+              <Image src={"/star.svg"} alt="star" width={22} height={22} />
+              <p className="text-sm text-light-500">
+                {feedback?.totalScore || "---"}/100
+              </p>
+            </div>
+          </div>
+
+          <p className="line-clamp-2 mt-5">
+            {feedback?.finalAssessment ||
+              "You haven't taken this interview yet. Take it now!"}
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between">
+          <DisplayTechIcons techStack={techstack} />
+          <Button className="btn primary">
+            <Link
+              href={
+                feedback
+                  ? `/interview/${interviewId}/feedback`
+                  : `/interview/${interviewId}`
+              }
+            >
+              {feedback ? "View Feedback" : "Take Interview"}
+            </Link>
+          </Button>
         </div>
       </div>
     </div>
