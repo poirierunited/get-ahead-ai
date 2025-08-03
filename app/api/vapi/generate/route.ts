fc35d0e5556edc94f15bacdcc501dba6f5141e30
Alejandro Poirier <ale.poirier16@gmail.com>
2025-08-03 18:01:36 -0400

    feat: add GET API endpoint to return a success message

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -45,3 +45,7 @@ export async function POST(request: Request) {
     return Response.json({ success: false, error: error }, { status: 500 });
   }
 }
+
+export async function GET() {
+  return Response.json({ success: true, data: "Thank you!" }, { status: 200 });
+}
